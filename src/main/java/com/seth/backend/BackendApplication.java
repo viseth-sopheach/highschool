15b3049e1982a6859a06f6baddd22e019c4d2129
Viseth <visethsopheach@gmail.com>
2026-09-09 13:38:52 +0700

    Initail commit

---
+++ src/main/java/com/seth/backend/BackendApplication.java
@@ -0,0 +1,18 @@
+package com.seth.backend;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+   public static void main(String[] args) {
+      Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+      dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+      SpringApplication.run(BackendApplication.class, args);
+   }
+
+}
+
